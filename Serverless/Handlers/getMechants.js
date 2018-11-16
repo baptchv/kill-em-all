a61413f78dc5ee45049480b82ffb8a655d5e95af
Baptiste Chevallier <bchevallier73170@gmail.com>
2018-11-16 20:41:19 +0100

    Modification des handlers avec ajout de mongoose

--- Serverless/Handlers/getMechants.js
+++ Serverless/Handlers/getMechants.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 
+const Villains = require('../Models/Town');
 
-const getHandler = async () => ({
-  status: 200,
-  body: JSON.stringify('Sucess')
-});
+mongoose.set('useNewUrlParser', true);
+//mongoose.connect('mongodb://localhost:27017/Villains');
+
+// let connection =
+// mongoose.createConnection('mongodb://localhost:27017/Villains');
+
+const getHandler = async () => {
+  const foo = await Villains.find({}).exec();
+  // console.log(foo.points);
+  // R.map pour gérer les bailds
+  console.log(foo);
+  return {
+    status: 200,
+    body: JSON.stringify('Success')
+  };
+};
 
 module.exports = {
   getHandler
---
+++ Serverless/Handlers/init.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const initiation = async () => {
+  // blabla initiation
+  return {
+    status: 200,
+    body: JSON.stringify('OK')
+  };
+};
+
+module.exports = {
+  initiation
+};
